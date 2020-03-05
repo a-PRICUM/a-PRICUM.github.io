7ad2cf3f318192b9c0d14f1f8527e4d2ceb22a2f
aPRICUM-h <hinatashuku0429@gmail.com>
2020-03-06 04:30:50 +0900

    changed URL

--- main.js
+++
@@ -1,9 +0,0 @@
-var myName = document.getElementById('my-name');
-var deg = 0;
-function rotateMyName(){
-  deg += 5;
-  deg %= 360;
-  myName.style.transform = 'rotateX(' + deg + 'deg)';
-}
-
-setInterval(rotateMyName, 20);
