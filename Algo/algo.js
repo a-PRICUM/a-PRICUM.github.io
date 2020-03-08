698364d4e4272faf0437d99b013d67a6037bb531
aPRICUM-h <hinatashuku0429@gmail.com>
2020-03-09 06:42:50 +0900

    j

--- Algo/algo.js
+++ Algo/algo.js
@@ -83,7 +83,7 @@ enterButton.onclick = ()=>{
         cardSerectArea.appendChild(passage2);
         */
         for(var i=0;i<24;i++){
-          if(person1[i] == person2[i]){
+          if(person1[i]===1 && person2===1){
             alert('Error 重複がありました。リロードして初めからやり直してください');
             break;
           }
